9e072fda337cc06c4ec2e9195f17d57adb9fa673
Jordan Cook <jordan@jc.me.uk>
2022-02-14 10:54:22 +0000

    Commited work so far

--- src/main/java/org/netjordan/ACMPCATLSArtifactsProvider/ACMPCATLSArtifactsProvider.java
+++ src/main/java/org/netjordan/ACMPCATLSArtifactsProvider/ACMPCATLSArtifactsProvider.java
@@ -1,28 +1,133 @@
 package org.netjordan.ACMPCATLSArtifactsProvider;
 
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.acmpca.AWSACMPCA;
-import com.amazonaws.services.acmpca.AWSACMPCAClientBuilder;
-import com.amazonaws.services.acmpca.model.InvalidNextTokenException;
-import com.amazonaws.services.acmpca.model.ListCertificateAuthoritiesRequest;
-import com.amazonaws.services.acmpca.model.ListCertificateAuthoritiesResult;
+import com.amazonaws.services.acmpca.AWSACMPCAClient;
+import com.amazonaws.services.acmpca.model.*;
 import com.amazonaws.services.elasticmapreduce.spi.security.TLSArtifacts;
 import com.amazonaws.services.elasticmapreduce.spi.security.TLSArtifactsProvider;
+import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
+import org.bouncycastle.asn1.x509.ExtendedKeyUsage;
+import org.bouncycastle.asn1.x509.Extension;
+import org.bouncycastle.asn1.x509.ExtensionsGenerator;
+import org.bouncycastle.asn1.x509.KeyPurposeId;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.bouncycastle.pkcs.PKCS10CertificationRequest;
+import org.bouncycastle.pkcs.PKCS10CertificationRequestBuilder;
+import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequestBuilder;
+
+import javax.security.auth.x500.X500Principal;
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.ByteBuffer;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ACMPCATLSArtifactsProvider extends TLSArtifactsProvider {
     AWSACMPCA awsAcmPce;
+    private static final long CERTIFICATE_VALIDITY_DAYS = 30L;
 
     public ACMPCATLSArtifactsProvider() {
-        this.awsAcmPce = AWSACMPCAClientBuilder.standard().build();
+        // TODO: use default region
+        this.awsAcmPce = AWSACMPCAClient.builder().withRegion(Regions.EU_WEST_2).build();
     }
 
     @Override
     public TLSArtifacts getTlsArtifacts() {
+        // get the first certificate authority
+        List<CertificateAuthority> certificateAuthorityList = listCertificateAuthorities();
+        CertificateAuthority certificateAuthority = certificateAuthorityList.get(0);
+
+        Validity validity = new Validity()
+                .withType("DAYS")
+                .withValue(ACMPCATLSArtifactsProvider.CERTIFICATE_VALIDITY_DAYS);
+
+        KeyPair keyPair = this.generateKeyPair();
+
+        ByteBuffer csr = null;
+        try {
+            csr = this.generateCSR(keyPair);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        IssueCertificateRequest issueCertificateRequest = new IssueCertificateRequest()
+                .withCertificateAuthorityArn(certificateAuthority.getArn())
+                .withSigningAlgorithm(SigningAlgorithm.SHA256WITHRSA)
+                .withValidity(validity)
+                .withCsr(csr)
+                .withIdempotencyToken("1234");
+
+        IssueCertificateResult issueCertificateResult = null;
+
+        try {
+            issueCertificateResult = awsAcmPce.issueCertificate(issueCertificateRequest);
+        } catch (Exception ex) {
+            throw ex;
+        }
+
+        // convert the returned certificate from a string into a List<Certificate>
+        List<X509Certificate> certificateList = this.convertToCertificateList(
+                this.getCertificate(issueCertificateResult.getCertificateArn())
+        );
+
+        TLSArtifacts tlsArtifacts = new TLSArtifacts(keyPair.getPrivate(), certificateList);
+
+
         return null;
     }
 
-    public void listCertificateAuthorities() {
-        ListCertificateAuthoritiesRequest listCertificateAuthoritiesRequest = new ListCertificateAuthoritiesRequest();
-        listCertificateAuthoritiesRequest.withMaxResults(1);
+    public String getCertificate(String certificateAuthorityArn) {
+        GetCertificateRequest getCertificateRequest = new GetCertificateRequest()
+                .withCertificateArn(certificateArn)
+                .withCertificateAuthorityArn(certificateAuthorityArn);
+
+        GetCertificateResult getCertificateResult = null;
+        try {
+            getCertificateResult = awsAcmPce.getCertificate(getCertificateRequest);
+        } catch (Exception ex) {
+            throw ex;
+        }
+
+        return null;
+    }
+
+    public List<X509Certificate> convertToCertificateList(String certificateString) {
+        // convert base64 pem encoded cert to Certificate
+        StringReader stringReader = new StringReader(certificateString);
+        PEMParser pemParser = new PEMParser(stringReader);
+        JcaX509CertificateConverter jcaX509CertificateConverter = new JcaX509CertificateConverter();
+
+        List<X509Certificate> x509CertificateList = new ArrayList<X509Certificate>();
+
+        Object object = null;
+
+        try {
+            while ((object = pemParser.readObject()) != null) {
+                if (object instanceof X509CertificateHolder) x509CertificateList.add(
+                        (X509Certificate) jcaX509CertificateConverter.getCertificate((X509CertificateHolder) object)
+                );
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return x509CertificateList;
+    }
+
+    public List<CertificateAuthority> listCertificateAuthorities() {
+        ListCertificateAuthoritiesRequest listCertificateAuthoritiesRequest = new ListCertificateAuthoritiesRequest()
+                .withResourceOwner(ResourceOwner.OTHER_ACCOUNTS);
+        listCertificateAuthoritiesRequest.withMaxResults(100);
 
         // Retrieve a list of your CAs.
         ListCertificateAuthoritiesResult listCertificateAuthoritiesResult = null;
@@ -33,6 +138,54 @@ public class ACMPCATLSArtifactsProvider extends TLSArtifactsProvider {
         }
 
         // Display the CA list.
-        System.out.println(listCertificateAuthoritiesResult.getCertificateAuthorities());
+        return listCertificateAuthoritiesResult.getCertificateAuthorities();
+    }
+
+    public KeyPair generateKeyPair() {
+        KeyPairGenerator keyPairGenerator = null;
+        try {
+            keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        }
+        catch (NoSuchAlgorithmException ex) {
+            ex.printStackTrace();
+        }
+
+        keyPairGenerator.initialize(2048, new SecureRandom());
+
+        return keyPairGenerator.generateKeyPair();
+    }
+
+    public ByteBuffer generateCSR(KeyPair keyPair) throws OperatorCreationException, IOException {
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+
+        // TODO: use region in the common name
+        X500Principal x500Principal = new X500Principal("CN=*.compute.internal.");
+
+        PKCS10CertificationRequestBuilder pkcs10CertificationRequestBuilder = new JcaPKCS10CertificationRequestBuilder(
+                x500Principal,
+                keyPair.getPublic()
+        );
+
+        ArrayList<KeyPurposeId> keyPurposeIds = new ArrayList<KeyPurposeId>();
+        keyPurposeIds.add(KeyPurposeId.id_kp_clientAuth);
+        keyPurposeIds.add(KeyPurposeId.id_kp_serverAuth);
+
+        ExtendedKeyUsage extendedKeyUsage = new ExtendedKeyUsage(keyPurposeIds.toArray(new KeyPurposeId[keyPurposeIds.size()]));
+
+        ExtensionsGenerator extensionsGenerator = new ExtensionsGenerator();
+        extensionsGenerator.addExtension(Extension.extendedKeyUsage, true, extendedKeyUsage);
+        pkcs10CertificationRequestBuilder.addAttribute(PKCSObjectIdentifiers.pkcs_9_at_extensionRequest, extensionsGenerator.generate());
+
+        JcaContentSignerBuilder jcaContentSignerBuilder = new JcaContentSignerBuilder("SHA256withRSA");
+        ContentSigner contentSigner = jcaContentSignerBuilder.build(keyPair.getPrivate());
+        PKCS10CertificationRequest pkcs10CertificationRequest = pkcs10CertificationRequestBuilder.build(contentSigner);
+
+//        StringWriter stringWriter = new StringWriter();
+//        JcaPEMWriter jcaPEMWriter = new JcaPEMWriter(stringWriter);
+//        jcaPEMWriter.writeObject(pkcs10CertificationRequest);
+//        jcaPEMWriter.close();
+//        System.out.println(stringWriter.getBuffer());
+
+        return ByteBuffer.wrap(pkcs10CertificationRequest.getEncoded());
     }
 }
--- src/test/java/org/netjordan/ACMPCATLSArtifactsProvider/test/ACMPCATLSArtifactsProviderTest.java
+++ src/test/java/org/netjordan/ACMPCATLSArtifactsProvider/test/ACMPCATLSArtifactsProviderTest.java
@@ -1,9 +1,16 @@
 package org.netjordan.ACMPCATLSArtifactsProvider.test;
 
+import com.amazonaws.services.acmpca.model.CertificateAuthority;
 import com.amazonaws.services.elasticmapreduce.spi.security.TLSArtifacts;
-import com.amazonaws.services.elasticmapreduce.spi.security.TLSArtifactsProvider;
+import org.bouncycastle.operator.OperatorCreationException;
 import org.junit.Test;
 import org.netjordan.ACMPCATLSArtifactsProvider.ACMPCATLSArtifactsProvider;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.security.KeyPair;
+import java.security.cert.X509Certificate;
+import java.util.List;
 
 public class ACMPCATLSArtifactsProviderTest {
     ACMPCATLSArtifactsProvider acmpcatlsArtifactsProvider;
@@ -14,13 +21,90 @@ public class ACMPCATLSArtifactsProviderTest {
 
     @Test
     public void testCanListPCAProviders() {
-        this.acmpcatlsArtifactsProvider.listCertificateAuthorities();
+        List<CertificateAuthority> certificateAuthorities = this.acmpcatlsArtifactsProvider.listCertificateAuthorities();
+
+        assert certificateAuthorities.size() > 0;
+
+        for (CertificateAuthority certificateAuthority : certificateAuthorities) {
+            System.out.println(certificateAuthority.getArn());
+        }
     }
 
     @Test
     public void testCanGetTLSArtifacts() {
+        TLSArtifacts tls = this.acmpcatlsArtifactsProvider.getTlsArtifacts();
 
-        TLSArtifacts tls = this.tlsArtifactsProvider.getTlsArtifacts();
+    }
+
+    @Test
+    public void testCanGenerateKeyPair() {
+        try {
+            KeyPair keyPair = this.acmpcatlsArtifactsProvider.generateKeyPair();
+        }
+        catch (Exception ex) {
+            assert false;
+        }
+    }
+
+
+    @Test
+    public void testCanGenerateCSR() {
+        try {
+            KeyPair keyPair = this.acmpcatlsArtifactsProvider.generateKeyPair();
+            ByteBuffer csr = this.acmpcatlsArtifactsProvider.generateCSR(keyPair);
+
+        } catch (OperatorCreationException | IOException e) {
+            e.printStackTrace();
+            assert false;
+        }
+    }
+
+    @Test
+    public void testCanConvertToCertificate() {
+        String certificateString = """
+           -----BEGIN CERTIFICATE-----
+           MIIFYjCCBEqgAwIBAgIQd70NbNs2+RrqIQ/E8FjTDTANBgkqhkiG9w0BAQsFADBX
+           MQswCQYDVQQGEwJCRTEZMBcGA1UEChMQR2xvYmFsU2lnbiBudi1zYTEQMA4GA1UE
+           CxMHUm9vdCBDQTEbMBkGA1UEAxMSR2xvYmFsU2lnbiBSb290IENBMB4XDTIwMDYx
+           OTAwMDA0MloXDTI4MDEyODAwMDA0MlowRzELMAkGA1UEBhMCVVMxIjAgBgNVBAoT
+           GUdvb2dsZSBUcnVzdCBTZXJ2aWNlcyBMTEMxFDASBgNVBAMTC0dUUyBSb290IFIx
+           MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAthECix7joXebO9y/lD63
+           ladAPKH9gvl9MgaCcfb2jH/76Nu8ai6Xl6OMS/kr9rH5zoQdsfnFl97vufKj6bwS
+           iV6nqlKr+CMny6SxnGPb15l+8Ape62im9MZaRw1NEDPjTrETo8gYbEvs/AmQ351k
+           KSUjB6G00j0uYODP0gmHu81I8E3CwnqIiru6z1kZ1q+PsAewnjHxgsHA3y6mbWwZ
+           DrXYfiYaRQM9sHmklCitD38m5agI/pboPGiUU+6DOogrFZYJsuB6jC511pzrp1Zk
+           j5ZPaK49l8KEj8C8QMALXL32h7M1bKwYUH+E4EzNktMg6TO8UpmvMrUpsyUqtEj5
+           cuHKZPfmghCN6J3Cioj6OGaK/GP5Afl4/Xtcd/p2h/rs37EOeZVXtL0m79YB0esW
+           CruOC7XFxYpVq9Os6pFLKcwZpDIlTirxZUTQAs6qzkm06p98g7BAe+dDq6dso499
+           iYH6TKX/1Y7DzkvgtdizjkXPdsDtQCv9Uw+wp9U7DbGKogPeMa3Md+pvez7W35Ei
+           Eua++tgy/BBjFFFy3l3WFpO9KWgz7zpm7AeKJt8T11dleCfeXkkUAKIAf5qoIbap
+           sZWwpbkNFhHax2xIPEDgfg1azVY80ZcFuctL7TlLnMQ/0lUTbiSw1nH69MG6zO0b
+           9f6BQdgAmD06yK56mDcYBZUCAwEAAaOCATgwggE0MA4GA1UdDwEB/wQEAwIBhjAP
+           BgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBTkrysmcRorSCeFL1JmLO/wiRNxPjAf
+           BgNVHSMEGDAWgBRge2YaRQ2XyolQL30EzTSo//z9SzBgBggrBgEFBQcBAQRUMFIw
+           JQYIKwYBBQUHMAGGGWh0dHA6Ly9vY3NwLnBraS5nb29nL2dzcjEwKQYIKwYBBQUH
+           MAKGHWh0dHA6Ly9wa2kuZ29vZy9nc3IxL2dzcjEuY3J0MDIGA1UdHwQrMCkwJ6Al
+           oCOGIWh0dHA6Ly9jcmwucGtpLmdvb2cvZ3NyMS9nc3IxLmNybDA7BgNVHSAENDAy
+           MAgGBmeBDAECATAIBgZngQwBAgIwDQYLKwYBBAHWeQIFAwIwDQYLKwYBBAHWeQIF
+           AwMwDQYJKoZIhvcNAQELBQADggEBADSkHrEoo9C0dhemMXoh6dFSPsjbdBZBiLg9
+           NR3t5P+T4Vxfq7vqfM/b5A3Ri1fyJm9bvhdGaJQ3b2t6yMAYN/olUazsaL+yyEn9
+           WprKASOshIArAoyZl+tJaox118fessmXn1hIVw41oeQa1v1vg4Fv74zPl6/AhSrw
+           9U5pCZEt4Wi4wStz6dTZ/CLANx8LZh1J7QJVj2fhMtfTJr9w4z30Z209fOU0iOMy
+           +qduBmpvvYuR7hZL6Dupszfnw0Skfths18dG9ZKb59UhvmaSGZRVbNQpsg3BZlvi
+           d0lIKO2d1xozclOzgjXPYovJJIultzkMu34qQb9Sz/yilrbCgj8=
+           -----END CERTIFICATE-----
+            """;
+
+        List<X509Certificate> certificateList = null;
+
+        try {
+            certificateList = this.acmpcatlsArtifactsProvider.convertToCertificatelist(certificateString);
+        }
+        catch (Exception ex) {
+            throw ex;
+        }
+
+        assert certificateList.size() > 0;
     }
 
 
